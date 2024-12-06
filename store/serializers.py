91cda17da8983113c8b29951cbeb4acacd7cbdac
Krishna Chaurasiya <chaurasiyakrishna731@gmail.com>
2024-12-06 19:19:45 +0530

    modifiying with model serializer

--- store/serializers.py
+++ store/serializers.py
@@ -4,21 +4,33 @@ from rest_framework import serializers
 from store.models import Product,Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title= serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Collection
+        fields=['id','title']
+
+    # id = serializers.IntegerField()
+    # title= serializers.CharField(max_length=255)
 
 #the role of the serializer file is to convert the upcoming data into json dictionary 
-class ProductSerializer(serializers.Serializer):
-    id=serializers.IntegerField()
-    title=serializers.CharField(max_length=255)
+# class ProductSerializer(serializers.Serializer):
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        #this will call from the model of the product & we dont have to write more no of codes
+        model = Product
+        fields = ['id','title','price','price_with_tax','collection']
+        # fields='__all__' for getting all from the model class
+
+
+    # id=serializers.IntegerField()
+    # title=serializers.CharField(max_length=255)
     price=serializers.DecimalField(max_digits=6,decimal_places=2,source='unit_price')#if the name is differaent from the modelsa we have to give source 
-    price_with_tax=serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name='collection_detail',
-        lookup_field='pk'
-    )
+    price_with_tax=serializers.SerializerMethodField(method_name='calculate_tax')# if want to change something we can write in the sub part of the modelserializer it will change the ui
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection_detail',
+    #     lookup_field='pk'
+    # )
     # collection =CollectionSerializer()#the name points the collection name
     # collection=serializers.StringRelatedField()#this will return the string name of the field
 
