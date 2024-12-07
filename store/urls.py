2aa3eb415fdec5658c66cbf8d94c91b87e6e3334
Krishna Chaurasiya <chaurasiyakrishna731@gmail.com>
2024-12-07 18:20:24 +0530

    Adding classs url replacing the function urls

--- store/urls.py
+++ store/urls.py
@@ -3,11 +3,13 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    # path('products/', views.product_list),
     path('products/', views.ProductList.as_view()),
-    # path('products/<int:id>/', views.product_detail),#to get only integer values
     path('products/<int:id>/', views.ProductDetail.as_view()),
-    path('collections/<int:pk>/', views.collection_detail,name='collection_detail'),#to get only integer values
-    path('collections/',views.collection_list)
+    path('collections/<int:pk>/', views.ColletionDetail.as_view()),
+    path('collections/',views.CollectionList.as_view())
+    # path('products/', views.product_list),
+    # path('products/<int:id>/', views.product_detail),#to get only integer values
+    # path('collections/<int:pk>/', views.collection_detail,name='collection_detail'),#to get only integer values
+    # path('collections/',views.collection_list),
 ]
 
