ad0080d09e88938163de2c524305ddb720fa99cf
Krishna Chaurasiya <chaurasiyakrishna731@gmail.com>
2024-12-08 16:18:33 +0530

    adding routers and nested routers for reviews

--- store/urls.py
+++ store/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
 from . import views
-from rest_framework.routers import SimpleRouter
-from pprint import pprint
 
-router=SimpleRouter()
-router.register('products',views.ProductViewSet)
+# from rest_framework.routers import SimpleRouter,DefaultRouter
+from django.urls.conf import include
+from rest_framework_nested import routers
+# from pprint import pprint
+
+router=routers.DefaultRouter()
+router.register('products',views.ProductViewSet,basename='products')
 router.register('collections',views.CollectionViewSet)
 # pprint(router.urls)
 
-urlpatterns=router.urls
+products_router=routers.NestedDefaultRouter(router,'products', lookup='product')
+products_router.register('reviews',views.ReviewViewSet,basename='product-reviews')
+urlpatterns=router.urls + products_router.urls
 
 
 # URLConf
