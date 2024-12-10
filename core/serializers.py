756243831ae878a366ef15c0312e43e78885366e
Krishna Chaurasiya <chaurasiyakrishna731@gmail.com>
2024-12-10 15:40:23 +0530

    adding attiributes of the user login

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,7 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from rest_framework import serializers
 
 class UserCreateSerializer(BaseUserCreateSerializer):
-    pass
+    birt_date=serializers.DateField()
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields=['id','username','password','email','first_name','last_name']
