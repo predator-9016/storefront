f758b27ba98e12bae068e99a2ed8e581d978a478
Krishna Chaurasiya <chaurasiyakrishna731@gmail.com>
2024-12-21 16:47:56 +0530

    Handlers for app

--- store/signals/handler.py
+++ store/signals/handler.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.conf import settings
 from django.dispatch import receiver
 
+#the function is called signal handler
 @receiver(post_save,sender=settings.AUTH_USER_MODEL)
 def create_customer_for_new_user(sender,**kwargs):
     if kwargs['created']:
